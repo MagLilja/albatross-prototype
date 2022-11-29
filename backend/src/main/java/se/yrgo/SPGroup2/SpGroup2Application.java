2b6c1e3aba542e25ae0d9e205406e27da2deae58
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-11-29 15:37:57 +0100

    created Spring backend

---
+++ backend/src/main/java/se/yrgo/SPGroup2/SpGroup2Application.java
@@ -0,0 +1,13 @@
+package se.yrgo.SPGroup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpGroup2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/se/yrgo/SPGroup2/SpGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.SPGroup2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpGroup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
