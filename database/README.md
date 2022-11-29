ff4804aae1f7778de9e7bfbeae07a741b2d2c2ad
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-11-29 16:23:53 +0100

    scaffolding database

--- database/README.md
+++ database/README.md
@@ -1,4 +1,4 @@
-# Spring boot
+# Postgres
 
 
 ## Project setup
---
+++ database/stop_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is used to stop a docker container
+
+# Stop the docker container
+docker stop postgres-group2
+
