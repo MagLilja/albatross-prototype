bd4f52d4d18a234d8718313fa755dfb7d278a4ac
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-11-30 10:08:14 +0100

    created master build bat script

---
+++ master_build.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+
+:: Build the backend
+cd backend
+call mvn clean install
+echo "################## Backend built ##################"
+echo "####################################################"
+
+
+:: Build the database docker image
+cd ..\database
+sh .\build_script.sh
+echo "################## Postgres built ##################"
+echo "####################################################"
+
+
+
+:: Build the frontend
+cd ..\frontend
+call npm install -g pnpm
+call pnpm install
+echo "################## Frontend built ##################"
+echo "####################################################"
+
+:: Build the reverse proxy
+cd ..\proxy
+call pnpm install
+echo "################## Proxy built ##################"
+echo "####################################################"
+
+
