f0ddbce0cec2a8a5f2d3f19d277543050f5011eb
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-07 19:42:00 +0100

    created navigation and started work on Home

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="bg-[#4a4a4b] w-full h-screen flex">
+            <div class="x basis-1/2 bg-"><img src="" alt="" srcset="../assets/bg-home.jpg" class="h-screen aspect-auto"></div>
+            <div class="x basis-1/2"></div>
+        </div>
+
+        HOME VIEW
+    </div>
+</template>
+
+<script>
+export default {
+    name: "test"
+};
+</script>
+
+<style scoped>
+
+.nav-links > li:hover {
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
---
+++ frontend/src/views/shop.vue
@@ -0,0 +1,19 @@
+<template>
+    <div >
+SHOP VIEW
+    </div>
+</template>
+
+<script>
+export default {
+    name: "test"
+};
+</script>
+
+<style scoped>
+
+.nav-links > li:hover {
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
