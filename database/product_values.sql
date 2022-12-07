78215980393865b9319e4582b3148814eeb2a60a
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-12-07 23:17:01 +0100

    WIP  adding data to product_values.sql

--- database/product_values.sql
+++ database/product_values.sql
@@ -1,16 +1,56 @@
-insert into product (id, art_num, type, model, size, color, price )
-    values
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
-        (default, "artNum", "type", "model", "size", "color", 1000),
+insert into product (id, art_num, type, model, size, color, price)
+values
+--        windbreakers
+(default, "0001", "Windbreaker", "Lysekil Unisex", "XS", "Navy", 890),
+(default, "0002", "Windbreaker", "Lysekil Unisex", "S", "Navy", 890),
+(default, "0003", "Windbreaker", "Lysekil Unisex", "M", "Navy", 890),
+(default, "0004", "Windbreaker", "Lysekil Unisex", "L", "Navy", 890),
+(default, "0005", "Windbreaker", "Lysekil Unisex", "XL", "Navy", 890),
+
+(default, "0006", "Windbreaker", "Lysekil Unisex", "XS", "Offwhite", 890),
+(default, "0007", "Windbreaker", "Lysekil Unisex", "S", "Offwhite", 890),
+(default, "0008", "Windbreaker", "Lysekil Unisex", "M", "Offwhite", 890),
+(default, "0009", "Windbreaker", "Lysekil Unisex", "L", "Offwhite", 890),
+(default, "0010", "Windbreaker", "Lysekil Unisex", "XL", "Offwhite", 890),
+
+(default, "0011", "Windbreaker", "Lysekil Unisex", "XS", "Grey", 890),
+(default, "0012", "Windbreaker", "Lysekil Unisex", "S", "Grey", 890),
+(default, "0014", "Windbreaker", "Lysekil Unisex", "M", "Grey", 890),
+(default, "0014", "Windbreaker", "Lysekil Unisex", "L", "Grey", 890),
+(default, "0015", "Windbreaker", "Lysekil Unisex", "XL", "Grey", 890),
+
+(default, "0016", "Windbreaker", "Lysekil Unisex", "XS", "Red", 890),
+(default, "0017", "Windbreaker", "Lysekil Unisex", "S", "Red", 890),
+(default, "0018", "Windbreaker", "Lysekil Unisex", "M", "Red", 890),
+(default, "0019", "Windbreaker", "Lysekil Unisex", "L", "Red", 890),
+(default, "0020", "Windbreaker", "Lysekil Unisex", "XL", "Red", 890),
+
+(default, "0021", "Windbreaker", "Lysekil Unisex", "XS", "Black", 890),
+(default, "0022", "Windbreaker", "Lysekil Unisex", "S", "Black", 890),
+(default, "0023", "Windbreaker", "Lysekil Unisex", "M", "Black", 890),
+(default, "0024", "Windbreaker", "Lysekil Unisex", "L", "Black", 890),
+(default, "0025", "Windbreaker", "Lysekil Unisex", "XL", "Black", 890),
+
+
+
+(default, "00", "Windbreaker", "Lysekil Junior", "XS", "Navy", 690),
+(default, "00", "Windbreaker", "Lysekil Junior", "XS", "Red", 690),
+(default, "00", "Windbreaker", "Lysekil Junior", "XS", "Black", 690),
+
+(default, "00", "Windbreaker", "Hunnebo Unisex", "XS", "Offwhite", 890),
+(default, "00", "Windbreaker", "Hunnebo Unisex", "XS", "Navy", 890),
+
+(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Navy", 990),
+(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Red", 990),
+(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Green", 990),
+(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Offwhite", 990),
+(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Grey", 990),
+
+(default, "00", "Windbreaker", "Lycke Unisex", "XS", "Red", 890),
+(default, "00", "Windbreaker", "Lycke Unisex", "XS", "Navy", 890),
+
+(default, "00", "Windbreaker", "Jacke Kungshamn", "XS", "Red", 890),
+(default, "00", "Windbreaker", "Jacke Kungshamn", "XS", "Black", 890),
+
 
 
