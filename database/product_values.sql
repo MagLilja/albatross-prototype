7673ec03b15a939d0254bfef52c121f6f322fa23
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-12-07 10:13:11 +0100

    creating data for product

---
+++ database/product_values.sql
@@ -0,0 +1,16 @@
+insert into product (id, art_num, type, model, size, color, price )
+    values
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+        (default, "artNum", "type", "model", "size", "color", 1000),
+
+
