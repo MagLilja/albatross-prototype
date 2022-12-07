32fb8a5f29b4d1a6a9d37e7548a65e640ea09bc5
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-12-07 23:34:51 +0100

    WIP  added junior size

--- backend/src/main/java/se/yrgo/SPGroup2/enums/ProductSize.java
+++ backend/src/main/java/se/yrgo/SPGroup2/enums/ProductSize.java
@@ -1,9 +1,18 @@
 package se.yrgo.SPGroup2.enums;
 
 public enum ProductSize {
+    //Adult Size
     XS,
     S,
     M,
     L,
-    XL
+    XL,
+
+    //Junior Size
+//    98/104,
+//    110/116,
+//    122/128,
+//    134/140,
+//    146/152,
+//    158/164
 }
