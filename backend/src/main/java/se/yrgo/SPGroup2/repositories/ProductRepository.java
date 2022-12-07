da98f53e405f4a616727202d2484ca2e988f8c90
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-07 09:37:23 +0100

    created impl of repo to product controller

--- backend/src/main/java/se/yrgo/SPGroup2/repositories/ProductRepository.java
+++ backend/src/main/java/se/yrgo/SPGroup2/repositories/ProductRepository.java
@@ -1,11 +1,16 @@
 package se.yrgo.SPGroup2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.yrgo.SPGroup2.domain.Product;
+import se.yrgo.SPGroup2.enums.ProductType;
 
+import java.util.List;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    public Product findByModel(String model);
+    public List<Product> findByType(ProductType type);
     public Product findByArtNum(String artNum);
 
 }
