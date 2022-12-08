12e383d39862543ed8f10cbf851c145dd28529f0
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-08 16:04:18 +0100

    added images to category view

---
+++ frontend/src/views/product.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Testar from "../components/Testar.vue"
+import router from "../router";
+
+
+</script>
+
+<template>
+    <div>
+
+        {{ $route.query.plan }}
+    </div>
+</template>
+
+
+<style scoped>
+
+.nav-links > li:hover {
+    text-decoration: underline;
+    cursor: pointer;
+}
+
+</style>
