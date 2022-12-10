05a30194d3bbc85ce9a8e5a4a0010c0ea84a4c39
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-10 18:57:28 +0100

    work on show product

--- frontend/src/views/admin/show-products.vue
+++ frontend/src/views/admin/show-products.vue
@@ -25,9 +25,27 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="px-16">
+    <div class="px-16 grid gap-x-4 grid-cols-[max-content_max-content_max-content_max-content_max-content_max-content_max-content]">
 
-        {{products}}
+        <div class="font-bold mb-2">Art. Nr.</div>
+        <div class="font-bold mb-2">Type</div>
+        <div class="font-bold mb-2">Model</div>
+        <div class="font-bold mb-2">Color</div>
+        <div class="font-bold mb-2">Price</div>
+        <div class="font-bold mb-2"></div>
+        <div class="font-bold mb-2"></div>
+
+        <div class="font-bold border-b-2 mb-2 col-span-7"></div>
+
+        <template v-for="product in products" >
+            <div class="x">{{ product.artNum }}</div>
+            <div class="x">{{ product.type }}</div>
+            <div class="x whitespace-nowrap">{{ product.model }}</div>
+            <div class="x">{{ product.color }}</div>
+            <div class="x">{{ product.price }},00 SEK</div>
+            <div class="text-[20px]">&#x1f5d1;</div>
+            <div class="text-[20px]">&#x1f58b;</div>
+        </template>
     </div>
 </template>
 
