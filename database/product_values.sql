fa6e52c986b30b80522ade54bcc30fc01497cf85
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-12-10 13:22:44 +0100

    insert values is complete for now

--- database/product_values.sql
+++ database/product_values.sql
@@ -1,6 +1,7 @@
 insert into product (id, art_num, type, model, size, color, price)
 values
 --        windbreakers
+--     Lysekil Unisex
 (default, "0001", "Windbreaker", "Lysekil Unisex", "XS", "Navy", 890),
 (default, "0002", "Windbreaker", "Lysekil Unisex", "S", "Navy", 890),
 (default, "0003", "Windbreaker", "Lysekil Unisex", "M", "Navy", 890),
@@ -32,25 +33,194 @@ values
 (default, "0025", "Windbreaker", "Lysekil Unisex", "XL", "Black", 890),
 
 
+-- Junior
+(default, "1000", "Windbreaker", "Lysekil Junior", "XS", "Navy", 690),
+(default, "2000", "Windbreaker", "Lysekil Junior", "XS", "Red", 690),
+(default, "3000", "Windbreaker", "Lysekil Junior", "XS", "Black", 690),
 
-(default, "00", "Windbreaker", "Lysekil Junior", "XS", "Navy", 690),
-(default, "00", "Windbreaker", "Lysekil Junior", "XS", "Red", 690),
-(default, "00", "Windbreaker", "Lysekil Junior", "XS", "Black", 690),
+-- Hunnebo Unisex
+(default, "0026", "Windbreaker", "Hunnebo Unisex", "XS", "Offwhite", 890),
+(default, "0027", "Windbreaker", "Hunnebo Unisex", "S", "Offwhite", 890),
+(default, "0028", "Windbreaker", "Hunnebo Unisex", "M", "Offwhite", 890),
+(default, "0029", "Windbreaker", "Hunnebo Unisex", "L", "Offwhite", 890),
+(default, "0030", "Windbreaker", "Hunnebo Unisex", "XL", "Offwhite", 890),
 
-(default, "00", "Windbreaker", "Hunnebo Unisex", "XS", "Offwhite", 890),
-(default, "00", "Windbreaker", "Hunnebo Unisex", "XS", "Navy", 890),
+(default, "0031", "Windbreaker", "Hunnebo Unisex", "XS", "Navy", 890),
+(default, "0032", "Windbreaker", "Hunnebo Unisex", "S", "Navy", 890),
+(default, "0033", "Windbreaker", "Hunnebo Unisex", "M", "Navy", 890),
+(default, "0034", "Windbreaker", "Hunnebo Unisex", "L", "Navy", 890),
+(default, "0035", "Windbreaker", "Hunnebo Unisex", "XL", "Navy", 890),
 
-(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Navy", 990),
-(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Red", 990),
-(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Green", 990),
-(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Offwhite", 990),
-(default, "00", "Windbreaker", "Jacke Bovall Unisex", "XS", "Grey", 990),
+-- Jacke Bovall Unisex
+(default, "0036", "Windbreaker", "Jacke Bovall Unisex", "XS", "Navy", 990),
+(default, "0037", "Windbreaker", "Jacke Bovall Unisex", "S", "Navy", 990),
+(default, "0038", "Windbreaker", "Jacke Bovall Unisex", "M", "Navy", 990),
+(default, "0039", "Windbreaker", "Jacke Bovall Unisex", "L", "Navy", 990),
+(default, "0040", "Windbreaker", "Jacke Bovall Unisex", "XL", "Navy", 990),
 
-(default, "00", "Windbreaker", "Lycke Unisex", "XS", "Red", 890),
-(default, "00", "Windbreaker", "Lycke Unisex", "XS", "Navy", 890),
+(default, "0041", "Windbreaker", "Jacke Bovall Unisex", "XS", "Red", 990),
+(default, "0042", "Windbreaker", "Jacke Bovall Unisex", "S", "Red", 990),
+(default, "0043", "Windbreaker", "Jacke Bovall Unisex", "M", "Red", 990),
+(default, "0044", "Windbreaker", "Jacke Bovall Unisex", "L", "Red", 990),
+(default, "0045", "Windbreaker", "Jacke Bovall Unisex", "XL", "Red", 990),
 
-(default, "00", "Windbreaker", "Jacke Kungshamn", "XS", "Red", 890),
-(default, "00", "Windbreaker", "Jacke Kungshamn", "XS", "Black", 890),
+(default, "0046", "Windbreaker", "Jacke Bovall Unisex", "XS", "Green", 990),
+(default, "0047", "Windbreaker", "Jacke Bovall Unisex", "S", "Green", 990),
+(default, "0048", "Windbreaker", "Jacke Bovall Unisex", "M", "Green", 990),
+(default, "0049", "Windbreaker", "Jacke Bovall Unisex", "L", "Green", 990),
+(default, "0050", "Windbreaker", "Jacke Bovall Unisex", "XL", "Green", 990),
 
+(default, "0051", "Windbreaker", "Jacke Bovall Unisex", "XS", "Offwhite", 990),
+(default, "0052", "Windbreaker", "Jacke Bovall Unisex", "S", "Offwhite", 990),
+(default, "0053", "Windbreaker", "Jacke Bovall Unisex", "M", "Offwhite", 990),
+(default, "0054", "Windbreaker", "Jacke Bovall Unisex", "L", "Offwhite", 990),
+(default, "0055", "Windbreaker", "Jacke Bovall Unisex", "XL", "Offwhite", 990),
 
+(default, "0056", "Windbreaker", "Jacke Bovall Unisex", "XS", "Grey", 990),
+(default, "0057", "Windbreaker", "Jacke Bovall Unisex", "S", "Grey", 990),
+(default, "0058", "Windbreaker", "Jacke Bovall Unisex", "M", "Grey", 990),
+(default, "0059", "Windbreaker", "Jacke Bovall Unisex", "L", "Grey", 990),
+(default, "0060", "Windbreaker", "Jacke Bovall Unisex", "XL", "Grey", 990),
 
+-- Lycke Unisex
+(default, "0061", "Windbreaker", "Lycke Unisex", "XS", "Red", 890),
+(default, "0062", "Windbreaker", "Lycke Unisex", "S", "Red", 890),
+(default, "0063", "Windbreaker", "Lycke Unisex", "M", "Red", 890),
+(default, "0064", "Windbreaker", "Lycke Unisex", "L", "Red", 890),
+(default, "0065", "Windbreaker", "Lycke Unisex", "XL", "Red", 890),
+
+(default, "0066", "Windbreaker", "Lycke Unisex", "XS", "Navy", 890),
+(default, "0067", "Windbreaker", "Lycke Unisex", "S", "Navy", 890),
+(default, "0068", "Windbreaker", "Lycke Unisex", "M", "Navy", 890),
+(default, "0069", "Windbreaker", "Lycke Unisex", "L", "Navy", 890),
+(default, "0070", "Windbreaker", "Lycke Unisex", "XL", "Navy", 890),
+
+-- Jacke Kungshamn
+(default, "0071", "Windbreaker", "Jacke Kungshamn", "XS", "Red", 890),
+(default, "0072", "Windbreaker", "Jacke Kungshamn", "S", "Red", 890),
+(default, "0073", "Windbreaker", "Jacke Kungshamn", "M", "Red", 890),
+(default, "0074", "Windbreaker", "Jacke Kungshamn", "L", "Red", 890),
+(default, "0075", "Windbreaker", "Jacke Kungshamn", "XL", "Red", 890),
+
+(default, "0076", "Windbreaker", "Jacke Kungshamn", "XS", "Black", 890),
+(default, "0077", "Windbreaker", "Jacke Kungshamn", "S", "Black", 890),
+(default, "0078", "Windbreaker", "Jacke Kungshamn", "M", "Black", 890),
+(default, "0079", "Windbreaker", "Jacke Kungshamn", "L", "Black", 890),
+(default, "0080", "Windbreaker", "Jacke Kungshamn", "XL", "Black", 890),
+
+-- Raincoats
+-- Ida Unisex
+(default, "0081", "Raincoat", "Ida Unisex", "XS", "Yellow", 790),
+(default, "0082", "Raincoat", "Ida Unisex", "S", "Yellow", 790),
+(default, "0083", "Raincoat", "Ida Unisex", "M", "Yellow", 790),
+(default, "0084", "Raincoat", "Ida Unisex", "L", "Yellow", 790),
+(default, "0085", "Raincoat", "Ida Unisex", "XL", "Yellow", 790),
+
+(default, "0086", "Raincoat", "Ida Unisex", "XS", "Red", 790),
+(default, "0087", "Raincoat", "Ida Unisex", "S", "Red", 790),
+(default, "0088", "Raincoat", "Ida Unisex", "M", "Red", 790),
+(default, "0089", "Raincoat", "Ida Unisex", "L", "Red", 790),
+(default, "0090", "Raincoat", "Ida Unisex", "XL", "Red", 790),
+
+(default, "0091", "Raincoat", "Ida Unisex", "XS", "Army Green", 790),
+(default, "0092", "Raincoat", "Ida Unisex", "S", "Army Green", 790),
+(default, "0093", "Raincoat", "Ida Unisex", "M", "Army Green", 790),
+(default, "0094", "Raincoat", "Ida Unisex", "L", "Army Green", 790),
+(default, "0095", "Raincoat", "Ida Unisex", "XL", "Army Green", 790),
+
+(default, "0096", "Raincoat", "Ida Unisex", "XS", "Dusty Pink", 790),
+(default, "0097", "Raincoat", "Ida Unisex", "S", "Dusty Pink", 790),
+(default, "0098", "Raincoat", "Ida Unisex", "M", "Dusty Pink", 790),
+(default, "0099", "Raincoat", "Ida Unisex", "L", "Dusty Pink", 790),
+(default, "0100", "Raincoat", "Ida Unisex", "XL", "Dusty Pink", 790),
+
+(default, "0101", "Raincoat", "Ida Unisex", "XS", "Navy", 790),
+(default, "0102", "Raincoat", "Ida Unisex", "S", "Navy", 790),
+(default, "0103", "Raincoat", "Ida Unisex", "M", "Navy", 790),
+(default, "0104", "Raincoat", "Ida Unisex", "L", "Navy", 790),
+(default, "0105", "Raincoat", "Ida Unisex", "XL", "Navy", 790),
+
+(default, "0106", "Raincoat", "Ida Unisex", "XS", "Black", 790),
+(default, "0107", "Raincoat", "Ida Unisex", "S", "Black", 790),
+(default, "0108", "Raincoat", "Ida Unisex", "M", "Black", 790),
+(default, "0109", "Raincoat", "Ida Unisex", "L", "Black", 790),
+(default, "0110", "Raincoat", "Ida Unisex", "XL", "Black", 790),
+
+-- Jackest
+-- Jacke Unisex
+
+(default, "0111", "Jacket", "Smögen Jacke Unisex", "XS", "White/Red", 2290),
+(default, "0112", "Jacket", "Smögen Jacke Unisex", "S", "White/Red", 2290),
+(default, "0113", "Jacket", "Smögen Jacke Unisex", "M", "White/Red", 2290),
+(default, "0114", "Jacket", "Smögen Jacke Unisex", "L", "White/Red", 2290),
+(default, "0115", "Jacket", "Smögen Jacke Unisex", "XL", "White/Red", 2290),
+
+(default, "0116", "Jacket", "Smögen Jacke Unisex", "XS", "White/Navy", 2290),
+(default, "0117", "Jacket", "Smögen Jacke Unisex", "S", "White/Navy", 2290),
+(default, "0118", "Jacket", "Smögen Jacke Unisex", "M", "White/Navy", 2290),
+(default, "0119", "Jacket", "Smögen Jacke Unisex", "L", "White/Navy", 2290),
+(default, "0120", "Jacket", "Smögen Jacke Unisex", "XL", "White/Navy", 2290),
+
+(default, "0121", "Jacket", "Smögen Jacke Unisex", "XS", "White/Light Blue", 2290),
+(default, "0122", "Jacket", "Smögen Jacke Unisex", "S", "White/Light Blue", 2290),
+(default, "0123", "Jacket", "Smögen Jacke Unisex", "M", "White/Light Blue", 2290),
+(default, "0124", "Jacket", "Smögen Jacke Unisex", "L", "White/Light Blue", 2290),
+(default, "0125", "Jacket", "Smögen Jacke Unisex", "XL", "White/Light Blue", 2290),
+
+(default, "0126", "Jacket", "Smögen Jacke Unisex", "XS", "White/Dark Grey", 2290),
+(default, "0127", "Jacket", "Smögen Jacke Unisex", "S", "White/Dark Grey", 2290),
+(default, "0128", "Jacket", "Smögen Jacke Unisex", "M", "White/Dark Grey", 2290),
+(default, "0129", "Jacket", "Smögen Jacke Unisex", "L", "White/Dark Grey", 2290),
+(default, "0130", "Jacket", "Smögen Jacke Unisex", "XL", "White/Dark Grey", 2290),
+
+-- Pants
+-- Jokkmokk Unisex
+(default, "0131", "Pants", "Jokkmokk Unisex", "XS", "Iron", 790),
+(default, "0132", "Pants", "Jokkmokk Unisex", "S", "Iron", 790),
+(default, "0133", "Pants", "Jokkmokk Unisex", "M", "Iron", 790),
+(default, "0134", "Pants", "Jokkmokk Unisex", "L", "Iron", 790),
+(default, "0135", "Pants", "Jokkmokk Unisex", "XL", "Iron", 790),
+
+(default, "0136", "Pants", "Jokkmokk Unisex", "XS", "Navy", 790),
+(default, "0137", "Pants", "Jokkmokk Unisex", "S", "Navy", 790),
+(default, "0138", "Pants", "Jokkmokk Unisex", "M", "Navy", 790),
+(default, "0139", "Pants", "Jokkmokk Unisex", "L", "Navy", 790),
+(default, "0140", "Pants", "Jokkmokk Unisex", "XL", "Navy", 790),
+
+(default, "0141", "Pants", "Jokkmokk Unisex", "XS", "Rusty", 790),
+(default, "0142", "Pants", "Jokkmokk Unisex", "S", "Rusty", 790),
+(default, "0143", "Pants", "Jokkmokk Unisex", "M", "Rusty", 790),
+(default, "0144", "Pants", "Jokkmokk Unisex", "L", "Rusty", 790),
+(default, "0145", "Pants", "Jokkmokk Unisex", "XL", "Rusty", 790),
+
+(default, "0146", "Pants", "Jokkmokk Unisex", "XS", "Black", 790),
+(default, "0147", "Pants", "Jokkmokk Unisex", "S", "Black", 790),
+(default, "0148", "Pants", "Jokkmokk Unisex", "M", "Black", 790),
+(default, "0149", "Pants", "Jokkmokk Unisex", "L", "Black", 790),
+(default, "0150", "Pants", "Jokkmokk Unisex", "XL", "Black", 790),
+
+-- West
+-- Koster Unisex
+(default, "0151", "West", "Koster Unisex", "XS", "White", 790),
+(default, "0152", "West", "Koster Unisex", "S", "White", 790),
+(default, "0153", "West", "Koster Unisex", "M", "White", 790),
+(default, "0154", "West", "Koster Unisex", "L", "White", 790),
+(default, "0155", "West", "Koster Unisex", "XL", "White", 790),
+
+(default, "0156", "West", "Koster Unisex", "XS", "Dark Grey", 790),
+(default, "0157", "West", "Koster Unisex", "S", "Dark Grey", 790),
+(default, "0158", "West", "Koster Unisex", "M", "Dark Grey", 790),
+(default, "0159", "West", "Koster Unisex", "L", "Dark Grey", 790),
+(default, "0160", "West", "Koster Unisex", "XL", "Dark Grey", 790),
+
+-- Accessories
+(default, "0161", "Accessories", "Hat", "", "Black", 150),
+(default, "0162", "Accessories", "Hat", "", "Navy", 150),
+
+(default, "0163", "Accessories", "Belt", "", "Dark Grey", 90),
+(default, "0164", "Accessories", "Belt", "", "Navy", 90),
+(default, "0165", "Accessories", "Belt", "", "White", 90),
+
+(default, "0166", "Accessories", "Cap", "", "Red", 150),
+(default, "0167", "Accessories", "Cap", "", "Navy", 150),
+(default, "0168", "Accessories", "Cap", "", "White", 150)
