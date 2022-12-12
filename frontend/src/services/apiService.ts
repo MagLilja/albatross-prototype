eebeb0e16e5d47373268cdb4c2aa2705668d4e10
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-12 18:30:34 +0100

    work on frontend auth

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -1,3 +1,5 @@
+import {useAuthStore} from "../stores/auth.store";
+
 function getHeaders() {
     const myHeaders = new Headers();
     myHeaders.append("cache", "no-cache");
@@ -13,11 +15,11 @@ const ApiService = {
 
         const requestOptions = {
             method: 'GET',
-            headers: headers ? headers : myHeaders,
+            headers: authHeader(endPoint),
         };
 
 
-        const promise = await fetch('/api/' + endPoint, requestOptions)
+        const promise = await fetch('/api/' + endPoint, requestOptions);
 
         if (promise.ok) {
             console.log(endPoint + ' fetched');
@@ -26,14 +28,17 @@ const ApiService = {
             throw new Error('Something went wrong when fetching data from ' + '/api/' + endPoint)
         }
     },
-    postDataTo: async function (endPoint: string, data: unknown, headers?: Headers): Promise<Response> {
+    postDataTo: async function (endPoint: string, body: unknown, headers?: Headers): Promise<Response> {
 
         const myHeaders = getHeaders();
 
         const requestOptions = {
             method: 'POST',
-            headers: headers ? headers : myHeaders,
-            body: JSON.stringify(data)
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: authHeader(endPoint)
+            },
+            body: JSON.stringify(body)
         };
 
         const promise = await fetch('/api/' + endPoint, requestOptions)
@@ -63,6 +68,21 @@ const ApiService = {
             throw new Error('Something went wrong when deleting data')
         }
     },
+}
+
+
+function authHeader(url: string) {
+    // return auth header with jwt if user is logged in and request is to the api url
+    const { user } = useAuthStore();
+    const isLoggedIn = !!user?.token;
+    const isApiUrl = url.startsWith("auth");
+    if (isLoggedIn && isApiUrl) {
+        console.log("isLoggedIn && isApiUrl");
+        return `Bearer ${user.token}`;
+    } else {
+        console.log("NOT isLoggedIn && isApiUrl");
+        return {};
+    }
 }
 
 export default ApiService
---
+++ frontend/src/stores/users.store.ts
@@ -0,0 +1,21 @@
+
+
+
+import {defineStore} from "pinia";
+
+const baseUrl = `/users`;
+
+export const useUsersStore = defineStore({
+    id: 'users',
+    state: () => ({
+        users: {}
+    }),
+    actions: {
+        async getAll() {
+            this.users = { loading: true };
+            fetchWrapper.get(baseUrl)
+                .then(users => this.users = users)
+                .catch(error => this.users = { error })
+        }
+    }
+});
