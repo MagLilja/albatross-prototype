89b477450491c1d84772964d1e529419f47d204e
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-12 18:30:34 +0100

    work on frontend auth

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import home from "../views/home.vue";
 import admin from "../views/admin.vue";
+import LoginView from "../views/auth/LoginView.vue";
 
 const router = createRouter({
     history: createWebHistory('/'),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: '/shop',
             name: 'shop',
             component: () => import('../views/shop.vue'),
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
         },
         {
             path: '/categories',
