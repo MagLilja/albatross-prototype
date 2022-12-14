9264bdae3819af1cca72fd721d63e98d15a3935e
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-14 20:01:53 +0100

    started on product_photo_insert_migration

--- backend/src/main/resources/db/migration/V2_3__product_photo_realtion.sql
+++ backend/src/main/resources/db/migration/V2_3__product_photo_realtion.sql
@@ -1,3 +1,11 @@
-insert into product_photo_list (product_id, photo_list_id )
-values
-(1,1)
+insert into public.product_photo_list (products_id, filename)
+values  (34, 'windbreakerHunneboUnisexNavy.webp'),
+        (35, 'windbreakerHunneboUnisexNavy.webp'),
+        (36, 'windbreakerHunneboUnisexNavy.webp'),
+        (37, 'windbreakerHunneboUnisexNavy.webp'),
+        (38, 'windbreakerHunneboUnisexNavy.webp'),
+        (29, 'windbreakerHunneboUnisexOffwhite.webp'),
+        (30, 'windbreakerHunneboUnisexOffwhite.webp'),
+        (31, 'windbreakerHunneboUnisexOffwhite.webp'),
+        (32, 'windbreakerHunneboUnisexOffwhite.webp'),
+        (33, 'windbreakerHunneboUnisexOffwhite.webp');
