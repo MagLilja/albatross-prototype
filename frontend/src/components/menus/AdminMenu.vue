3ebd10a95feb0cd9f54a7f759658f0cf2a49a788
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-16 15:44:52 +0100

    work on frontend admin side

--- frontend/src/components/menus/AdminMenu.vue
+++ frontend/src/components/menus/AdminMenu.vue
@@ -1,27 +1,32 @@
 <script setup lang="ts">
 
 
+import {useAuthStore} from "../../stores/authStore";
+
 const props = defineProps<{
     atTop: boolean
 }>()
+
+const auth = useAuthStore()
+
 
 </script>
 
 
 <template>
     <ul class="nav-links flex gap-2 text-white" :class="{'text-black':props.atTop}">
-        <li>
+        <li class="font-bold">
+            Admin menu:
+        </li>        <li>
             <router-link to="/admin/add-product">Add product</router-link>
         </li>
         <li>
             <router-link to="/admin/show-products">Show products</router-link>
         </li>
         <li>
-            <router-link to="/about">Orders</router-link>
+            <a @click.prevent="auth.logout()"> - Logout</a>
         </li>
-        <li>
-            <router-link to="/impressions">Impressions</router-link>
-        </li>
+
     </ul>
 </template>
 
