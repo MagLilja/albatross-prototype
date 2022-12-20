790e856f88f9b4a1bc1556cd0c272c19bb8aec03
Magnus Lilja <magnus.lilja@consiliumsafety.com>
2022-12-20 09:34:32 +0100

    added some javadoc

--- backend/src/main/java/se/yrgo/SPGroup2/enums/RoleEnum.java
+++ backend/src/main/java/se/yrgo/SPGroup2/enums/RoleEnum.java
@@ -1,6 +1,9 @@
 package se.yrgo.SPGroup2.enums;
 
+
 public enum RoleEnum {
   ROLE_USER,
   ROLE_ADMIN
+
+
 }
--- backend/src/main/java/se/yrgo/SPGroup2/jsonWebToken/AuthEntryPointJwt.java
+++ backend/src/main/java/se/yrgo/SPGroup2/jsonWebToken/AuthEntryPointJwt.java
@@ -15,12 +15,23 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * This class is used to handle unauthorized requests. It will return a 401 error code (Unauthorized) to the client.
+ */
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
   private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
-
+  /**
+   * This method is called whenever an exception is thrown due to an unauthenticated user trying to access a
+   * resource that requires authentication.
+   * @param request
+   * @param response
+   * @param authException
+   * @throws IOException
+   * @throws ServletException
+   */
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
     logger.error("Unauthorized error: {}", authException.getMessage());
